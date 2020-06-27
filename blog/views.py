f920b1e6203599e5cd7a461f968181b3b31b57e4
zengjiapei3000 <41148275+zengjiapei3000@users.noreply.github.com>
2020-06-27 22:57:15 +0800

    06/27/22:25 Fixed views, urls and templates of 4 customize template tags, going to current view when click the button.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Category, Tag
 import markdown
 import re
 from django.utils.text import slugify
@@ -24,3 +24,26 @@ def detail(request, pk):
     post.toc = m.group(1) if m is not None else ''
 
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(
+        created_time__year=year,
+        created_time__month=month).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(
+        category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def tag(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tag=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+    
+
+    
