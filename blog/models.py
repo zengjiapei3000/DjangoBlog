af58e39703c63a40bbd68a5bf6ab66e8fa7af3d8
zengjiapei3000 <41148275+zengjiapei3000@users.noreply.github.com>
2020-07-01 22:45:28 +0800

    07/01/22:45 Added the article reading statistics.

--- blog/models.py
+++ blog/models.py
@@ -31,6 +31,7 @@ class Tag(models.Model):
     再次强调一定要继承 models.Model 类！
     """
     name = models.CharField(max_length=100)
+    #post = models.ManyToManyField(Post, related_name='tag', null=True)
 
     class Meta:
         verbose_name = '标签'
@@ -38,7 +39,6 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-
 
 
 class Post(models.Model):
@@ -56,9 +56,11 @@ class Post(models.Model):
     excerpt = models.CharField('摘要', max_length=200, blank=True)
 
     category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    tag = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
 
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    views = models.PositiveIntegerField(default=0, editable=False)
 
     class Meta:
         verbose_name = '文章'
@@ -88,6 +90,13 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+
+
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,9 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    
+    post.increase_views()
+
     md = markdown.Markdown(extensions=[
                     'markdown.extensions.extra',
                     'markdown.extensions.codehilite',
